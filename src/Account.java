bec35e335cc6c97b514f01fe1d555ca971ffa40d
srikanthpragada <srikanthpragada@gmail.com>
2016-12-05 08:15:49 +0530

    5-DEC

---
+++ src/Account.java
@@ -0,0 +1,11 @@
+
+public class Account {
+
+ 	 // instance variables 
+	 private int acno;
+	 private String customer;
+	 private double balance;
+	 
+	 // methods 
+	 
+}
---
+++ src/TestAccount.java
@@ -0,0 +1,16 @@
+
+public class TestAccount {
+
+	public static void main(String[] args) {
+		 
+		Account a1;  // object reference 
+		
+		  a1 = new Account();  // create object 
+		  // a1.acno = 100; 
+		  
+		
+		
+
+	}
+
+}
