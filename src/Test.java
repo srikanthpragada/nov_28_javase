cdd654e544c4b2f97658f2088cc9455105c45acb
srikanthpragada <srikanthpragada@gmail.com>
2016-12-15 08:06:25 +0530

    15-DEC

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,22 @@
 public class Test {
 
 	public static void main(String[] args) {
-	 
-		    System.out.println("Welcome to Eclipse");
+	   int num = 10;
+	   
+       fun(num); // auto-boxing
+	   fun(10.50);
+		   
 	}
 
+	public static void fun(Object obj) {
+		if (obj instanceof Integer) {
+		    int num =  (Integer) obj;
+		    System.out.println( num + 1);
+		}
+		else
+		{
+			double num =  (Double) obj;
+			System.out.println(num + 0.1);
+		}
+	}
 }
